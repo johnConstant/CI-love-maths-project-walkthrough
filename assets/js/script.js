14cfa57a51c12e5b8ce125763d9faecf6dfdbfd1
John Constant <johnconstantis@gmail.com>
2022-06-12 21:33:54 +0000

    Add basic Javascript structure of functions

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function startGame(){}
+
+function checkAnswer(){}
+
+function calculateAnswer(){}
+
+function incrementScore(){}
+
+function incrementWrongAnswer(){}
+
+function displayAdditionQuestion(){}
+
+function displaySubtractionQuestion(){}
+
+function displayMultiplicationQuestion(){}
+
+function displayDivisionQuestion(){}
